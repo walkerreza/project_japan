b27b079bb32f158c9d583a55c127e43076dfad82
anka reza <rezawalker02@gmail.com>
2026-07-11 10:15:22 +0700

    Detach japanlingov2 from parent repository

--- japanlingov2/resources/js/Components/Navigation/ApplicationLogo.jsx
+++
@@ -1,9 +0,0 @@
-import { Link } from '@inertiajs/react';
-
-export default function ApplicationLogo({ className = '' }) {
-    return (
-        <Link href="/" className="flex items-center gap-2 no-underline">
-            <img src="/logo.png" alt="Japanlingo" className={`h-10 w-auto ${className}`} />
-        </Link>
-    );
-}
